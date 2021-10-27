32569af553b82b06a86eebcb442a761da48fa392
Neevash Ramdial <mail@neevash.dev>
2021-10-27 12:43:41 -0600

    react project clean up

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,22 @@ import Cookies from "universal-cookie";
 
 import Auth from "./components/Auth"
 import ChannelContainer from "./components/ChannelContainer";
+require("dotenv").config();
 
-const apiKey = "stream_api_key";
-
-const client = StreamChat.getInstance(apiKey);
+const client = StreamChat.getInstance(process.env.REACT_APP_STREAM_API_KEY);
 const cookies = new Cookies();
-
 const authToken = cookies.get('token')
 
-if(authToken) {
+if (authToken) {
   client.connectUser({
     id: cookies.get('userId'),
     name: cookies.get("name"),
   }, authToken);
 }
 
-
-
 const LogoutButton = ({ logout }) => (<button onClick={logout}>Logout</button>);
 
 const GetCal = ({ getCal }) => {
-
   return (
     <div>
       <button onClick={getCal}>Get Cal</button>
@@ -35,47 +30,49 @@ const GetCal = ({ getCal }) => {
 }
 
 const App = () => {
-
   const logout = (response) => {
+    // TODO: use localStorage
     // Logs out of app by removing token from cookies
-
     cookies.remove('token');
     cookies.remove('name');
     cookies.remove('userId');
     cookies.remove('code');
     cookies.remove('googleToken');
-    
 
     window.location.reload();
   }
 
-  const getCal = () => {
-    const token = cookies.get("googleToken")
-    const URL = 'https://www.googleapis.com/calendar/v3/calendars/primary/events';
+  const getCal = async () => {
+    try {
+      const token = cookies.get("googleToken")
+      const url = 'https://www.googleapis.com/calendar/v3/calendars/primary/events';
 
-    fetch(URL, {
-      method: 'GET',
-      headers: {
-        "Content-type": "application/json",
-        "Authorization": `Bearer ${token}`
-      }
-    }).then((res)=>res.json()).then((data)=> { 
-      console.log(data)
-    })
+      const res = await fetch(url, {
+        method: 'GET',
+        headers: {
+          "Content-type": "application/json",
+          "Authorization": `Bearer ${token}`
+        }
+      }).json();
+
+      console.log("GET GCAL RESPONSE", res);
+    } catch (err) {
+      console.error(err);
+      throw new Error(err);
+    }
   }
 
   if (!authToken) return <Auth />
-  
 
   return (
     <div className="app__wrapper">
-    <Chat client={client}>
-        <LogoutButton logout={logout}/>
-        <GetCal getCal={getCal}/>
-      <div>
-        <ChannelContainer />
-      </div>
-    </Chat>
+      <Chat client={client}>
+        <LogoutButton logout={logout} />
+        <GetCal getCal={getCal} />
+        <div>
+          <ChannelContainer />
+        </div>
+      </Chat>
     </div>
   );
 }
--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,7 +1,8 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Cookies from "universal-cookie";
 import axios from "axios";
 import GoogleLogin from "react-google-login";
+require("dotenv").config();
 
 
 const cookies = new Cookies();
@@ -14,59 +15,28 @@ const initialState = {
   confirmPassword: "",
 }
 
-const clientId = "google_client_id"
+const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
 
 const Auth = () => {
   const [form, setForm] = useState(initialState);
   const [isSignup, setIsSignup] = useState(true);
 
-  // const handleChange = (e) => {
-  //   setForm({ ...form, [e.target.name]: e.target.value});
-
-  // }
-
-  // const switchMode = () => {
-  //   setIsSignup((prevIsSignup) => !prevIsSignup)
-  // }
-
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-
-  //   const { fullName, username, password, email } = form;
-    
-  //   const URL = `http://localhost:3001/auth/${isSignup ? 'signup' : 'login'}`;
-  //   // Send data to backend 
-  //   const { data: { token, userId, hashedPassword } } = await axios.post(`${URL}`, {
-  //     fullName, username, password, email
-  //   });
-  //   cookies.set('token', token);
-  //   cookies.set('userId', userId);
-  //   cookies.set('fullName', fullName);
-  //   cookies.set('username', username);
-
-  //   if(isSignup) {
-  //     cookies.set('hashedPassword', hashedPassword);
-  //   }
-
-  //   window.location.reload();
-
-  // }
-
   const responseGoogle = (googleRes) => {
     const data = googleRes.profileObj;
-    console.log(googleRes)
-    const URL = `http://17c30c451fca.ngrok.io/auth/login`;
+    console.clear()
+    console.log(`Google auth response ${googleRes}`)
+    const URL = `http://localhost:3001/auth/login`;
 
 
     fetch(URL, {
       method: 'POST',
       headers: {
         'Content-type': 'application/json'
-      }, 
-      body: JSON.stringify({data: data, token: googleRes.tokenId})
+      },
+      body: JSON.stringify({ data: data, token: googleRes.tokenId })
     }).then((res) => res.json()).then((userData) => {
-      
+
       const { name, email, token, userId, url } = userData;
       console.log(userData)
       cookies.set('name', name);
@@ -75,105 +45,27 @@ const Auth = () => {
       cookies.set('userId', userId);
       window.location.assign(url)
       // window.location.reload();
-      
-      
+
+
     });
   }
 
-    return (
-        <div className="auth__form-container">
-          {/* <div className="auth__form-container_fields">
-            <div className="auth__form-container_fields-content">
-            <p>{isSignup ? 'Sign Up With Email' : 'Sign In'}</p>
-              <form onSubmit={handleSubmit}>
-              {isSignup && (
-                <div className="auth__form-container_fields-content_input">
-                  
-                  <label htmlFor="fullName">Full Name</label>
-                  <input 
-                    name="fullName"
-                    type="text"
-                    placeholder="Full Name"
-                    onChange={handleChange}
-                    required
-                  />
-                </div>
-                )}
-                {isSignup && (
-                <div className="auth__form-container_fields-content_input">
-                  <label htmlFor="email">Email</label>
-                  <input 
-                    name="email"
-                    type="text"
-                    placeholder="Email"
-                    onChange={handleChange}
-                    required
-                  />
-                </div>
-                )}
-                
-                <div className="auth__form-container_fields-content_input">
-                  <label htmlFor="username">Username</label>
-                  <input 
-                    name="username"
-                    type="text"
-                    placeholder="Username"
-                    onChange={handleChange}
-                    required
-                  />
-                </div>
-                
-                <div className="auth__form-container_fields-content_input">
-                  <label htmlFor="password">Password</label>
-                  <input 
-                    name="password"
-                    type="password"
-                    placeholder="Password"
-                    onChange={handleChange}
-                    required
-                  />
-                </div>
-               
-                {isSignup && (
-                <div className="auth__form-container_fields-content_input">
-                  <label htmlFor="confirmPassword">Confirm Password</label>
-                  <input 
-                    name="confirmPassword"
-                    type="password"
-                    placeholder="Confirm Password"
-                    onChange={handleChange}
-                    required
-                  />
-                </div>
-                )}
-                <div className="auth__form-container_fields-content_button">
-                            <button>{isSignup ? "Sign Up" : "Sign In"}</button>
-                        </div>
-              </form>
-                <p>
-                  {isSignup
-                  ? "Already have an account?"
-                  : "Don't have an account?"}
-                    <span onClick={switchMode}>
-                        {isSignup ? 'Sign In' : 'Sign Up'}
-                    </span>
-                </p>
-            </div>
-          </div> */}
-          <span>Sign up With Google:
-          <div>
-            <GoogleLogin 
-              clientId={clientId}
-              buttonText="Login"
-              onSuccess={responseGoogle}
-              onFailure={responseGoogle}
-              cookiePolicy="single_host_origin"
-            
-            />
-          </div>
-          </span>
+  return (
+    <div className="auth__form-container">
+      <span>Sign up With Google:
+        <div>
+          <GoogleLogin
+            clientId={clientId}
+            buttonText="Login"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy="single_host_origin"
+
+          />
         </div>
-      )
+      </span>
+    </div>
+  )
 }
 
 export default Auth;
